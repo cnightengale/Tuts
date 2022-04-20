26725e687d306744fc624c2dc9e0768a79204900
cnightengale <cnightengale@ivytech.edu>
2022-04-19 22:23:29 -0400

    M08

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
-// register view engine
-app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const blogs = [
@@ -18,6 +22,7 @@ app.get('/', (req, res) => {
   ];
   res.render('index', { title: 'Home', blogs });
 });
+
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
